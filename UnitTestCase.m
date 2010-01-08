304a0d3bb01ea3a212884bf54a28c4dc97a950c9
Jake Kring <kringj@carleton.edu>
2010-01-08 12:36:23 -0600

    Jake added a bunch of assertions to the the Unit Test Case. Check 'em out!

--- UnitTestCase.m
+++ UnitTestCase.m
@@ -25,6 +25,29 @@
 
 - (void) testLayerManager {
 	GNLayerManager *manager = [GNLayerManager sharedManager];
+	NSString *landmarkName = @"Boliou";
+	GNLandmark *newLandmark = [[GNLandmark landmarkWithID:1 name:landmarkName latitude:(CLLocationDegrees)15.0 longitude:(CLLocationDegrees)20.0] retain];
+	GNLayer *academicBuildings = [GNLayer layerWithName:@"Academic Buildings"];
+	GNLayer *food = [GNLayer layerWithName:@"Food"];
+	GNLayer *administration = [GNLayer layerWithName:@"Administration"];
+	[manager addLayer:academicBuildings];
+	[manager addLayer:food active:NO];
+	[manager addLayer:administration];
+	[manager setLayer:administration active:YES];
+	STAssertTrue([newLandmark getNumActiveLayers] == 0, @"The new Landmark should NOT be associated with any layers");
+	[newLandmark addActiveLayer:academicBuildings];
+	STAssertTrue([newLandmark getNumActiveLayers] == 1, @"The Landmark should be associated with 1 layer: academic buildings");
+	[newLandmark addActiveLayer:food];
+	[newLandmark addActiveLayer:administration];
+	STAssertTrue([newLandmark getNumActiveLayers] == 3, @"The Landmark should be associated with 3 layers: academic buildings, food and administration");
+	[newLandmark addActiveLayer:food];
+	STAssertTrue([newLandmark getNumActiveLayers] == 3, @"After adding a redundant layer, the landmark should still be associated with only 3 layers: academic buildings, food and administration");
+	[newLandmark removeActiveLayer:food];
+	STAssertTrue([newLandmark getNumActiveLayers] == 2, @"After removing what was a redundant layer, the landmark should now be associated with 2 layers: academic buildings and administration");
+	[newLandmark removeActiveLayer:food];
+	STAssertTrue([newLandmark getNumActiveLayers] == 2, @"After removing a non-existent layer, the landmark should still be associated with 2 layers: academic buildings and administration");
+	[newLandmark clearActiveLayers];
+	STAssertTrue([newLandmark getNumActiveLayers] == 0, @"After removing all layers, the landmark shouldn't be associated with any layers");
 	[manager release];
 }
 
