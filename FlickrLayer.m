3d36e3cdd2ee6c892f94c4f2b51da9d051be461c
KringJ <KringJ@carleton.edu>
2010-02-22 22:02:06 -0600

    Made the background pretty.

--- FlickrLayer.m
+++ FlickrLayer.m
@@ -193,6 +193,7 @@
 
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
+	self.view.backgroundColor = [UIColor groupTableViewBackgroundColor];
 	[super viewDidLoad];
 }
 
